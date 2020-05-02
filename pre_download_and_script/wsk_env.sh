b82bc889d97312d467d52615da25a104d463341a
Heersin <56885667+Heersin@users.noreply.github.com>
2020-05-02 14:21:54 +0800

    update script and dockerfile
    Update
    - Docker file
    - script for building
    - some pre downloaded files

---
+++ pre_download_and_script/wsk_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export OW_DB=CouchDB
+export OW_DB_USERNAME=openwhisk
+export OW_DB_PASSWORD=openwhisk
+export OW_DB_PROTOCOL=http
+export OW_DB_HOST=127.0.0.1
+export OW_DB_PORT=4444
+export OPENWHISK_TMP_DIR=/home/wsk_logconf/
